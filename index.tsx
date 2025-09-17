5108f48ddcd39773fcd83397aa71fb399737e159
Taya <31591090+tayadev@users.noreply.github.com>
2025-09-17 12:36:43 +0200

    add favicon

--- index.tsx
+++ index.tsx
@@ -10,6 +10,7 @@ const Skeleton: React.FC<React.PropsWithChildren<{}>> = ({ children }) => (
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link rel="stylesheet" href="/.assets/styles.css" />
+            <link rel="icon" href="/.assets/favicon.png" />
         </head>
         <body>
             {children}
